9bb973a68b3b47b4f54d582268269192b15b1c17
Mark Johnson <mark@barrenfrozenwasteland.com>
2023-06-20 22:49:59 +0100

    Add packaging script

---
+++ package.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Package necessary files for upload to extensions.gnome.org
+zip advancedvolumecontrol@mark.johnson.ubuntu.com extension.js metadata.json README.md screenshot.webp stylesheet.css
